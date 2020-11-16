3a25a4962cec4926ec3fa476e96eecc0530a03c3
Malte Böhm <boemalte@gmail.com>
2020-11-16 09:52:10 +0100

    - Add new backend

--- backend/src/main/java/com/secretallergy/secretallergy/SecretAllergyApplication.java
+++ backend/src/main/java/com/secretallergy/secretallergy/SecretAllergyApplication.java
@@ -1,4 +1,4 @@
-package secretallergy.com.demo;
+package com.secretallergy.secretallergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecretAllergyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SecretAllergyApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SecretAllergyApplication.class, args);
+	}
 
 }
--- backend/src/test/java/com/secretallergy/secretallergy/SecretAllergyApplicationTests.java
+++ backend/src/test/java/com/secretallergy/secretallergy/SecretAllergyApplicationTests.java
@@ -1,4 +1,4 @@
-package secretallergy.com.demo;
+package com.secretallergy.secretallergy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecretAllergyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
