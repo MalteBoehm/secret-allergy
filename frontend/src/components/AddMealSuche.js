c8f9bf329e1b39e83ae5e1cf9cc52f774544b1db
Malte Böhm <boemalte@gmail.com>
2020-11-25 17:20:15 +0100

    - add Live Search logic -- added for Backlog wait logic to use the api only if the user hasnt typed for 1.5 seconds

--- frontend/src/components/AddMealSuche.js
+++ frontend/src/components/AddMealSuche.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import { Row} from "react-native-easy-grid";
 import { SearchBar } from 'react-native-elements';
+import {LiveSearchContext} from "../context/LiveSearchContext";
+import SearchInputContext from "../context/SearchInputContext";
 
 export default function AddMealSuche(){
 
-    const [mealName, setMealName] = useState();
+    const {searchInput, setSearchInput} = useContext(SearchInputContext);
+
+
+    const handler = (input=> {
+        setSearchInput(input)
+    });
 
     return(
-
             <Row size={0.5}
                  style={{
                         display:'flex',
@@ -17,8 +23,10 @@ export default function AddMealSuche(){
 
 
                 <SearchBar placeholder={'Suche ein Produkt'}
+                           preventDefault
                            autoCorrect={false}
-                           value={mealName}
+                           value={searchInput}
+                           onChangeText={handler}
                            containerStyle={{backgroundColor: 'gray' ,width:'100%'}}
                            inputContainerStyle={{backgroundColor: 'white', }}/>
             </Row>
