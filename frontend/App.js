7ccdd12bd3a130974071064a097cc706a6ff4d08
Malte Böhm <boemalte@gmail.com>
2020-11-25 08:59:42 +0100

    - add context for livesearch to AddMealList

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import Navigator from "./src/navigation/Navigator";
-
+import LiveSearchProvider from "./src/context/LiveSearchProvider";
 
 
 
@@ -9,7 +9,9 @@ import Navigator from "./src/navigation/Navigator";
 
 export default function App() {
   return (
-      <Navigator/>
+      <LiveSearchProvider>
+            <Navigator/>
+      </LiveSearchProvider>
         )
       }
 
