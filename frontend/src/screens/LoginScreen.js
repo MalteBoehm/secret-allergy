03402657dc2283acad519382ad66a9505c357baf
Malte Böhm <boemalte@gmail.com>
2020-11-27 12:23:43 +0100

    - add ActivityIndicator (Loading) -- add useState that indicates for 1000ms that it is loading -- add useState for token

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, View} from 'react-native';
 import LoginHeader from "../components/LoginScreen/LoginHeader";
 import LoginForm from "../components/LoginScreen/LoginForm";
 
 export default function LoginScreen ({ navigation }){
 
+    const [isLoading, setIsLoading] = useState(true)
+    const [userToken, setUserToken] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000)
+    }, []);
+
+    if ( isLoading ) {
+        return(
+            <View style={{
+                        flex:1,
+                        justifyContent: 'center',
+                        alignItems: 'center'}}
+            >
+                <ActivityIndicator size='large'/>
+            </View>
+        )
+    }
     return(
         <View>
             <LoginHeader/>
