586af2470b65fc89b4350f08e41f9a2a9c8ac936
Malte Böhm <boemalte@gmail.com>
2020-11-27 11:55:58 +0100

    - add react native elements loginForm Styling -> no function yet

--- frontend/src/components/LoginScreen/LoginForm.js
+++ frontend/src/components/LoginScreen/LoginForm.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View} from 'react-native';
+import {Input} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function LoginForm(){
 
     return(
-        <Text h2>Login Form</Text>
+        <View>
+            <Input
+                placeholder='Username'
+                leftIcon={
+                    <Icon
+                        name='user'
+                        size={24}
+                        color='black'
+                    />
+                }
+            />
+            <Input
+                placeholder='Password'
+                errorStyle={{ color: 'red' }}
+                errorMessage='password is wrong'
+                secureTextEntry={true}
+                onChangeText={value =>
+                    alert('Password')}
+            />
+
+        </View>
     )
 }
