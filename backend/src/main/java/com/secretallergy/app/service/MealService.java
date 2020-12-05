41dd66d631d94d87770f9ca8659f18e3ae607f98
Malte Boehm <boemalte@gmail.com>
2020-12-05 12:22:29 +0100

    - fixed better clearer allergenList to frontend CU-b0u3pd

--- backend/src/main/java/com/secretallergy/app/service/MealService.java
+++ backend/src/main/java/com/secretallergy/app/service/MealService.java
@@ -22,6 +22,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Service
@@ -40,9 +43,11 @@ public class MealService {
         this.idUtils = idUtils;
     }
 
-    public List<Product> searchProductsByNameService(String productName) throws  UnirestException {
+
+    public List<Product> searchProductsByNameService(String productName) throws UnirestException {
         return openFoodFactsApi.searchProductByName(productName);
-        }
+    }
+
 
     public Meal addMealToUser(AddMealDto addMealDto) {
         LocalDate date = LocalDate.now();
@@ -52,34 +57,87 @@ public class MealService {
 
         return mealMongo.save(
                 Meal.builder()
-                    .mealOfUserId(addMealDto.getUserId())
-                    .mealId(idUtils.generateId())
-                    .date(date.format(currentDate))
-                    .mealDaytime(addMealDto.getMealParam())
-                    .products(addMealDto.getAddMealListOfProducts())
-                    .allergens( addAllergens(addMealDto) )
-                    .hasSideEffect(false)
-                    .sideEffects(emptyListOfSideEffects)
-                    .ratingOfSideEffects(0)
-                .build());
+                        .mealOfUserId(addMealDto.getUserId())
+                        .mealId(idUtils.generateId())
+                        .date(date.format(currentDate))
+                        .mealDaytime(addMealDto.getMealParam())
+                        .products(addMealDto.getAddMealListOfProducts())
+                        .allergens(addAllergens(addMealDto))
+                        .hasSideEffect(false)
+                        .sideEffects(emptyListOfSideEffects)
+                        .ratingOfSideEffects(0)
+                        .build());
+    }
+
+    public List<Meal> getTodayMealsOfUser(String user) {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter currentDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return mealMongo.findMealByDateIsAndMealOfUserId(date.format(currentDate), user);
     }
 
 
-/* Helper Functions */
-    public List<Allergen> addAllergens(AddMealDto addMealDto){
-
+    /* Helper Functions */
+    public List<Allergen> addAllergens(AddMealDto addMealDto) {
         List<Allergen> allergens = new ArrayList<>();
+
         addMealDto.getAddMealListOfProducts().forEach(product -> {
             for (int i = 0; i < product.getIngredients_text_de().size(); i++) {
-                List<String> newList = Arrays.asList(product.getIngredients_text_de().get(i));
-                for (String searchWord: newList) {
-                    allergens.addAll(allergenMongo.findAllergenByName("^"+searchWord));
+
+
+                List<String> ingredientsOfProduct = filterIngredients(List.of(product.getIngredients_text_de().get(i)));
+                for (String searchWord : ingredientsOfProduct) {
+                    allergens.addAll(allergenMongo.findAllergensByNames(searchWord));
                 }
             }
         });
         return allergens;
     }
+
+
+    private List<String> filterIngredients(List<String> ingredientsOfProduct) {
+        List<String> listOfFilteredIngredients = new ArrayList<>();
+        ArrayList<String> filterWords = new ArrayList<>(List.of(
+                "Teig",
+                "Konservierungsstoff",
+                "Stabilisator",
+                "Antioxidationsmittel",
+                "Gesamtmilchbestandteile",
+                " im ",
+                " Produkt",
+                "Überzugsmittel",
+                "Gesamtkakaobestandteile",
+                "aufgeschlossenes",
+                "Gesamtfettanteil",
+                "davon",
+                "Pflanzenfett",
+                "Käse",
+                "fettarmes",
+                "Pflanzenfett",
+                "aufgeschlossenes",
+                "Pflanzeneiweiß",
+                "eingelegte",
+                "raffiniertes",
+                "frittierte",
+                "",
+                ":",
+                ";",
+                ".",
+                "_",
+                "  ",
+                ")",
+                "(",
+                "%",
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Wasser", "Gesamtfettgehalt", "davon", "und","aufgeschlossenes"));
+
+        for (String ingredient : ingredientsOfProduct) {
+            System.out.println(ingredientsOfProduct +"");
+            for (String filterWord : filterWords) {
+                if (!ingredient.strip().equalsIgnoreCase(filterWord)) {
+                    listOfFilteredIngredients.add(ingredient);
+                }
+            }
+        }
+        return listOfFilteredIngredients.stream().distinct().collect(Collectors.toList());
+    }
 }
-
-
 
