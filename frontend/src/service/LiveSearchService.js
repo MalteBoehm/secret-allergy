9ee8a25415f55d9af3d447ad0458193c206f0154
Malte Boehm <boemalte@gmail.com>
2020-12-08 12:14:03 +0100

    - add new updateSideEffectsInMeal to update the Meal in the backend

--- frontend/src/service/LiveSearchService.js
+++ frontend/src/service/LiveSearchService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const header = (token) => ({
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
 });
 
 
@@ -17,16 +17,23 @@ export const getLiveSearchData = (searchInput, token) =>
 
 const mealUrl = "http://192.168.178.41:8080/api/meal/new";
 export const createMeal = (userId, mealParam, addMealListOfProducts, token) => {
-  axios
-    .post(mealUrl, { userId, mealParam, addMealListOfProducts }, header(token))
-    .then((response) => response.data);
+    axios
+      .post(mealUrl, { userId, mealParam, addMealListOfProducts }, header(token))
+      .then((response) => response.data);
 };
 
 
 const getAllMealsUrl = "http://localhost:8080/api/meal/getall?user=";
 export const getAllTodayMeals = (userId, token) =>
   axios
-    .get(getAllMealsUrl + userId , header(token))
+    .get(getAllMealsUrl + userId, header(token))
     .then((response) => response.data)
     .catch((error) => console.log(error));
 
+
+const updateSideEffectsInMealUrl = "http://192.168.178.41:8080/api/meal/sideeffectupdate";
+export const updateSideEffectsInMeal = (userId, createdSideEffectDto, token) => {
+    axios
+      .put(createSideEffectsToMealUrl, { userId, createdSideEffectDto }, header(token))
+      .then((response) => response.data);
+};
