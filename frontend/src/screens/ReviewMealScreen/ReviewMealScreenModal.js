a0b5f61012858e64b4db5727adc48896c9a80a17
Malte Boehm <boemalte@gmail.com>
2020-12-08 12:19:08 +0100

    - add styling - add context instead of local useStates of hasSideEffectIsSelected and sideEffectsList

--- frontend/src/screens/ReviewMealScreen/ReviewMealScreenModal.js
+++ frontend/src/screens/ReviewMealScreen/ReviewMealScreenModal.js
@@ -1,68 +1,60 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Grid, Row, Col } from "react-native-easy-grid";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Button, CheckBox, Input, ListItem } from "react-native-elements";
 import Slider from "@react-native-community/slider";
+import DashboardContext from "../../context/DashboardContext";
 
 export default function ReviewMealScreenModal() {
-    const [isSelected, setSelection] = useState(false);
-    const toggleChecked = () => setSelection(value => !value);
 
-    const [sideEffectsList, setSideEffectsList] = useState([]);
+    const { hasSideEffectIsSelected, setHasSideEffectIsSelected } = useContext(DashboardContext);
+    const toggleChecked = () => setHasSideEffectIsSelected(value => !value);
+
+    const { sideEffectsList, setSideEffectsList } = useContext(DashboardContext);
     const [singleSideEffect, setSingleSideEffect] = useState("");
-
-    const hasSideffects = () => {
-        if (isSelected) {
-            return 1;
-        } else {
-            return 0;
-        }
-    };
     const [ratingOfSideEffect, setRatingOfSideEffect] = useState(0);
 
-    function handleRemove(id) {
-        const newList = sideEffectsList.filter((item) => item.id !== id);
-        setSideEffectsList(newList);
-    }
-    const handleCreateSideEffectInList = ()=> {
-        if (sideEffectsList !== sideEffectsList[sideEffectsList.length - 1] && ratingOfSideEffect !== 0) {
+
+    const handleCreateSideEffectInList = () => {
+        if (sideEffectsList !== sideEffectsList[sideEffectsList.length - 1] && ratingOfSideEffect !== 0 && singleSideEffect.length > 2) {
             const singleSideEffectWithRating = {
                 sideEffect: singleSideEffect,
-                rating: ratingOfSideEffect
+                ratingOfSideEffects: ratingOfSideEffect
             };
             setSideEffectsList(sideEffectsList => [...sideEffectsList, singleSideEffectWithRating]);
             setSingleSideEffect("");
             setRatingOfSideEffect(0);
         }
-    }
+    };
 
     return (
       <Grid>
+          <Row size={0.3} style={styles.checkboxHeading}>
+              <Text style={styles.checkboxHeading}>Gab es Beschwerden durch die Mahlzeit?</Text>
+          </Row>
           <Row size={1}>
               <View style={styles.container}>
                   <View style={styles.checkboxContainer}>
                       <CheckBox
                         center
                         title="Hattest du Beschwerden?"
-                        checked={isSelected}
+                        checked={hasSideEffectIsSelected}
                         onPress={toggleChecked}
                       />
-
-                      <Text style={styles.label}>Gab es Beschwerden durch die Mahlzeit?</Text>
                   </View>
               </View>
           </Row>
           <Row size={4}>
               <Col size={3}>
-
-                  {!isSelected ?
-                    <Text>232 Fall du keine Beschwerden hattest klicke oben rechts auf "Beschwerden hinzufügen"</Text> :
+                  {!hasSideEffectIsSelected ?
+                    <Text>Falls du keine Beschwerden hattest klicke oben rechts auf "Beschwerden hinzufügen" damit du
+                        keine lücken im Tagebuch hast!</Text> :
                     <View>
                         <Input
-                      placeholder="Eine Beschwerde hinzufügen"
-                      onChangeText={text => setSingleSideEffect(text)}
-                      maxLength={40}
-                    />
+                          placeholder="Eine Beschwerde hinzufügen"
+                          onChangeText={text => setSingleSideEffect(text)}
+                          maxLength={40}
+                        />
                         <Slider
                           style={{ width: "50%", height: 40 }}
                           minimumValue={0}
@@ -85,7 +77,7 @@ export default function ReviewMealScreenModal() {
                         height: "90%",
                         width: "100%"
                     }}
-                    keyExtractor={item => item.toString()}
+                    keyExtractor={item => item.sideEffect.toString()}
                     data={sideEffectsList}
                     contentContainerStyle={{ height: "100%", width: "100%" }}
                     renderItem={({ item }) => {
@@ -93,15 +85,16 @@ export default function ReviewMealScreenModal() {
                           <View>
                               <ListItem containerStyle={{ flex: 1, padding: 4, height: 80 }}>
                                   <ListItem.Content style={{ alignSelf: "center" }}>
-                                      <Text style={{}}>{`\u2022`} {item.sideEffect} als Beschwerde, auf einer Skala von 1 bis 10
-                                          bei: {item.rating}</Text>
+                                      <Text style={{}}>{`\u2022`} {item.sideEffect} als Beschwerde, auf einer Skala von
+                                          1 bis 10
+                                          bei: {item.ratingOfSideEffects}</Text>
                                   </ListItem.Content>
                               </ListItem>
                           </View>);
                     }} />
               </Col>
               <Col size={1}>
-                  {!isSelected ?
+                  {!hasSideEffectIsSelected ?
                     <Text> </Text>
                     :
                     <View>
@@ -117,10 +110,13 @@ export default function ReviewMealScreenModal() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center"
-
-
+    },
+    checkboxHeading: {
+        alignSelf: "center",
+        fontWeight: "bold"
     },
     checkboxContainer: {
         flexDirection: "row",
