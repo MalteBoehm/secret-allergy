d965fced7fe1da44005c263c6e0e58f1351133fb
Malte Boehm <boemalte@gmail.com>
2020-12-13 16:55:39 +0100

    - add new Filterwords - add splitUpperCaseWords helper functions - add more logic to cleanlist CU-bcwfdy

--- backend/src/main/java/com/secretallergy/app/service/MealService.java
+++ backend/src/main/java/com/secretallergy/app/service/MealService.java
@@ -41,6 +41,7 @@ public class MealService {
 
 
     public Meal addMealToUser(AddMealDto addMealDto) {
+
         val date = LocalDate.now();
         val currentDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
@@ -91,7 +92,7 @@ public class MealService {
                             .product_name(product.getProduct_name())
                             .brands(product.getBrands())
                             .image_front_thumb_url(product.getImage_front_thumb_url())
-                            .ingredients_text_de(filterIngredients(filteredIngredientsOfSingleProduct)).build();
+                            .ingredients_text_de(filteredIngredientsOfSingleProduct).build();
                     cleanListOfProducts.add(newProduct);
                 }
         );
@@ -106,13 +107,13 @@ public class MealService {
             for (int i = 0; i < product.getIngredients_text_de().size(); i++) {
                 val ingredientsOfProduct = filterIngredients(List.of(product.getIngredients_text_de().get(i)));
                 for (String searchWord : ingredientsOfProduct) {
-                    Optional<List<Allergen>> searchAllergensInDb = Optional.of(allergenMongo.findAllergensByNamesMatchesRegex(searchWord.trim()));
+                    val searchAllergensInDb = Optional.of(allergenMongo.findAllergensByNamesMatchesRegex(searchWord.trim()));
 
                     for (int j = 0; j < searchAllergensInDb.get().size(); j++) {
                         if (searchAllergensInDb.get().get(j).getNames().get(0).equals("")) {
                             return;
                         } else {
-                            System.out.println(allergens.add(searchAllergensInDb.get().get(j)));
+                            allergens.add(searchAllergensInDb.get().get(j));
                         }
                     }
                 }
@@ -124,46 +125,31 @@ public class MealService {
 
     private List<String> filterIngredients(List<String> ingredientsOfProduct) {
         val filterWords = new ArrayList<>(List.of(
-                "Teig",
-                "Konservierungsstoff",
-                "Stabilisator",
-                "Antioxidationsmittel",
-                "Gesamtmilchbestandteile",
-                " im ",
-                " Produkt",
-                "Überzugsmittel",
-                "Gesamtkakaobestandteile",
-                "aufgeschlossenes",
-                "Gesamtfettanteil",
-                "davon",
-                "Pflanzenfett",
-                "Käse",
-                "fettarmes",
-                "Pflanzenfett",
-                "aufgeschlossenes",
-                "Pflanzeneiweiß",
-                "eingelegte",
-                "raffiniertes",
-                "frittierte",
-                "",
-                ":",
-                ";",
-                ".",
-                "_",
-                "  ",
-                ")",
-                "(",
-                "%",
-                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "UKäseMilch", "Wasser", "Gesamtfettgehalt", "davon", "und", "aufgeschlossenes", "     ", "Säuerungsmittel"));
+                "Teig", "Konservierungsstoff", "Konservierungsstoffe",
+                "Stabilisator", "Stabilisatoren", "Antioxidationsmittel", "Antioxidationsmitteln",
+                "Gesamtmilchbestandteile", "im", "von", " Produkt", "Überzugsmittel",
+                "Gesamtkakaobestandteile", "aufgeschlossenes", "Gesamtfettanteil",
+                "davon", "Pflanzenfett", "Käse", "fettarmes", "Pflanzenfett", "Fisch",
+                "aufgeschlossenes", "Pflanzeneiweiß", "eingelegte", "raffiniertes", "frittierte", "halbierte", "gehobelter",
+                "UKäseMilch", "Wasser", "Gesamtfettgehalt", "davon", "und", "aufgeschlossenes", "     ", "Kräuter",
+                "Säuerungsmittel", "enthalten", "Spuren", "Kann", "gegrillte", "Gewürze", "gU", "Krebstieren und Soja enthalten",
+                "", ":", ";", ".", "_", ")", "(", "%",
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
 
         List<String> checkForDuplicatesList = new ArrayList<>();
-        for (String ingredient : ingredientsOfProduct) {
-            String cleanedIngredient = ingredient.replaceAll("^[\s]w*|(\s{2,})w*|$([\s])w*", "");
-            for (String filterWord : filterWords) {
-                if (!cleanedIngredient.equalsIgnoreCase(filterWord)) {
-                    if (cleanedIngredient.length() > 3) {
-                        if (!checkForDuplicatesList.contains(cleanedIngredient)) {
-                            System.out.println(checkForDuplicatesList.add(cleanedIngredient.trim()));
+// Check IngredientsOfProductsList for UppercaseUppercase, but a , and split it to new list /todo
+        val upperCaseWordsSplitIngredientsList = splitUpperCaseWordsToListItems(ingredientsOfProduct);
+
+        for (val ingredient : upperCaseWordsSplitIngredientsList) {
+            val singleCleanedIngredient = ingredient.replaceAll("^[\s]w*|(,\s)w*|(\s{2,})w*|$([\s])w*", "");
+
+            // Compares every ingredient to each filter word
+            for (val filterWord : filterWords) {
+                if (!singleCleanedIngredient.equalsIgnoreCase(filterWord)) {
+                    if (singleCleanedIngredient.length() > 3) {
+                        if (!checkForDuplicatesList.contains(singleCleanedIngredient)) {
+                            var ingredientWithNoFilterWord = singleCleanedIngredient.replaceAll(filterWord, "");
+                            checkForDuplicatesList.add(ingredientWithNoFilterWord);
                         }
                     }
                 }
@@ -172,10 +158,23 @@ public class MealService {
         val uniqueList = new ArrayList<>(checkForDuplicatesList);
         List<String> cleanList = new ArrayList<>();
         for (String item : uniqueList) {
-            if (!item.isEmpty()) {
+            if (!item.isEmpty() && !filterWords.contains(item)) {
                 cleanList.add(item);
             }
         }
         return cleanList;
+    }
+
+
+
+    private List<String> splitUpperCaseWordsToListItems(List<String> listOfIngredients) {
+        List<String> ingredientListWithSplitUpperCase = new ArrayList<>();
+        for (var word : listOfIngredients) {
+            for (int i = 1; i < word.length(); i++) {
+                List<String> splitUpperCaseWordArray = Arrays.asList(word.replaceAll("([a-z])([A-Z])", "$1,$2").split(","));
+                ingredientListWithSplitUpperCase.addAll(splitUpperCaseWordArray);
+            }
+        }
+        return ingredientListWithSplitUpperCase;
     }
 }
