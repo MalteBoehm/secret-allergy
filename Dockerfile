47414d458f2e51d233ce158f0d3ecb410fb51ce4
Malte Boehm <boemalte@gmail.com>
2020-12-14 20:06:28 +0100

    - add API env variable

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ MAINTAINER Malte Böhm <boemalte@gmail.com>
 
 ADD backend/target/secret-allergy.jar app.jar
 
-CMD ["sh" , "-c", "java -jar -Dserver.port=$PORT -Djwt.secretkey=$JWT_SECRETKEY -Dspring.data.mongodb.uri=$MONGO_DB_URI app.jar"]
+CMD ["sh" , "-c", "java -jar -Dserver.port=$PORT -Djwt.secretkey=$JWT_SECRETKEY -Dspring.data.mongodb.uri=$MONGO_DB_URI -Dopen.food.facts.api.key=$OPEN_FOOD_FACTS_API_KEY app.jar"]
--- backend/src/main/java/com/secretallergy/app/api/OpenFoodFactsApi.java
+++ backend/src/main/java/com/secretallergy/app/api/OpenFoodFactsApi.java
@@ -5,47 +5,44 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import com.secretallergy.app.model.Product;
 import com.secretallergy.app.utils.IdUtils;
 import lombok.val;
-import org.apache.http.client.HttpResponseException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 
 @Service
 public class OpenFoodFactsApi {
 
-    private IdUtils idUtils;
+    private final IdUtils idUtils;
+    private final String openFoodFactsApiKey;
 
 
     @Autowired
-    public OpenFoodFactsApi(IdUtils idUtils) {
+    public OpenFoodFactsApi(IdUtils idUtils, @Value("${OPEN_FOOD_FACTS_API_KEY}") String openFoodFactsApiKey) {
         this.idUtils = idUtils;
+        this.openFoodFactsApiKey = openFoodFactsApiKey;
     }
 
     public List<Product> searchProductByName(String productName) throws UnirestException {
 
         JSONArray products = getUniRestRequest(productName);
-        List<Product> productList = new ArrayList<>();
+        val productList = new ArrayList<Product>();
         for (int i = 0; i < products.length(); i++) {
-            var jsonObject = products.getJSONObject(i);
+            val jsonObject = products.getJSONObject(i);
             String id = getProductIdIfPossibleOrGiveOwnId(jsonObject);
             String name = getProductNameIfPossible(jsonObject);
             String brands = getBrandIfPossible(jsonObject);
             String imageUrl = getProductPictureIfPossible(jsonObject);
-            ArrayList<String> ingredients_text_de = new ArrayList<>( getIngredientsIfPossible(jsonObject) );
-            productList.add(new Product(id, name, brands, ingredients_text_de, imageUrl));
+            val ingredients_text_de = new ArrayList<>( getIngredientsIfPossible(jsonObject) );
+            productList.add( new Product(id, name, brands, ingredients_text_de, imageUrl));
         }
         return productList;
     }
@@ -81,8 +78,7 @@ public class OpenFoodFactsApi {
     private String getProductPictureIfPossible(JSONObject productObject) {
         try {
             return productObject.keySet().contains("image_url") ?
-                    productObject.getString("image_url") :
-                    productObject.getString("image_front_url");
+                    productObject.getString("image_url") : productObject.getString("image_front_url");
         } catch (JSONException e) {
             return "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png";
         }
@@ -104,7 +100,7 @@ public class OpenFoodFactsApi {
         return Unirest.get("https://de.openfoodfacts.org/cgi/search.pl?search_terms=" + productName.replace(" ", "%20") + "&sort_by=unique_scans_n&json=true")
                 .header("Accept", "application/json")
                 .header("User-Agent", "Secret-Allergy")
-                .header("Authorization", "Basic bWFsdGViOlhjWVczMTgxMQ==")
+                .header("Authorization", openFoodFactsApiKey)
                 .asJson()
                 .getBody()
                 .getObject()
